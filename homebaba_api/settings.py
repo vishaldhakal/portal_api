119192e97312e44f37acb81c65bd965ba72ace4d
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-01 11:22:41 +0545

    sDV

--- homebaba_api/settings.py
+++ homebaba_api/settings.py
@@ -81,9 +81,9 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = None
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portal',
-        'USER': 'portal_admin',
-        'PASSWORD': 'DatabaseUserPassword',
+        'NAME': 'condomonk',
+        'USER': 'condomonk_admin',
+        'PASSWORD': 'condomonkadminpassword',
         'HOST': 'localhost',
         'PORT': '',
     }
