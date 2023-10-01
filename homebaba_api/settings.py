12f1447408969cf10c23e08305dc6b5fa9040d80
vishaldhakal <vishaldhakal96@gmail.com>
2023-10-01 10:59:12 +0545

    asDV

--- homebaba_api/settings.py
+++ homebaba_api/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'portal',
-        'USER': 'portal_admin',
-        'PASSWORD': 'portaladminpassword',
+        'USER': 'vishal',
+        'PASSWORD': 'DatabaseUserPassword',
         'HOST': 'localhost',
         'PORT': '',
     }
